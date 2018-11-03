3bbb5c3c3fe64dbdced1b492b0194910f0b2ac8c
tedshd <tedshd@gmail.com>
2018-11-03 15:27:40 +0800

    [dev] Remove log.

--- carousel_banner.js
+++ carousel_banner.js
@@ -141,21 +141,17 @@
 
             if ('ontouchstart' in document.documentElement) {
                 dom.addEventListener('touchstart', function (e) {
-                    console.log('touchstart');
                     intervalManager(false);
                 });
                 dom.addEventListener('touchend', function (e) {
-                    console.log('touchend');
                     intervalManager(true, fadeTimer, time);
                 });
             } else {
                 dom.addEventListener('mouseenter', function (e) {
-                    console.log('mouseenter');
                     intervalManager(false);
                 });
 
                 dom.addEventListener('mouseleave', function (e) {
-                    console.log('mouseleave');
                     intervalManager(true, fadeTimer, time);
                 });
             }
